d4de44291ffc30b5c7821cdd085edbfe180e1bec
Raphael Mazzieri <rquitto@gmail.com>
2024-05-18 22:08:21 -0300

    refactor: funcionalidades refatoradas

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 const addButton = document.getElementById('botao-adicionar');
 const resultContainer = document.getElementById('resultado');
 
-// Evento do botão adicionar
+// Evento do botão adicionar tarefa
 addButton.addEventListener("click", () => {
     createTask();
 });
 
 const createTask = () => {
-    // Obtendo o valor digitado pelo usuário
     const newTaskText = document.getElementById('tarefa-input').value;
+    // Evitar adicionar tarefas vazias
+    if (newTaskText.trim() === ''){
+        alert("Nenhuma tarefa detectada!")
+        return 
+    };
 
     // Criando o elemento label que irá envolver elementos html da tarefa
     const taskLabel = document.createElement('label');
     taskLabel.innerHTML = `
-        <input type="checkbox" class="checkbox-marked">${newTaskText}
+        <input type="checkbox" class="checkbox-marked">
+        <span class="task-text">${newTaskText}</span>
         <button class="editButton">Editar</button>
         <button class="deleteButton">Deletar</button>
     `;
 
-    // Função deletar tarefa
-    deleteTask(taskLabel);
-
-    // Função editar tarefa
-    editTask(taskLabel);
-
-    // Função concluir tarefa
-    finishTask(taskLabel);
+    deleteTask(taskLabel);  // Função deletar tarefa
+    editTask(taskLabel); // Função editar tarefa
+    finishTask(taskLabel);// Função concluir tarefa
 
     // Adicionando o elemento label criado dinamicamente ao documento html
     resultContainer.appendChild(taskLabel);
+    document.getElementById('tarefa-input').value = ''; // Limpa o campo após adicionar a tarefa
 };
 
 // Deletar tarefa
 const deleteTask = (taskLabel) => {
-    // Obtendo o botão de deletar dentro da tarefa
+    // Capturando o botão de deletar dentro da tarefa
     const deleteButton = taskLabel.querySelector('.deleteButton');
     deleteButton.addEventListener("click", () => {
         taskLabel.remove();
@@ -44,7 +45,38 @@ const deleteTask = (taskLabel) => {
 const editTask = (taskLabel) => {
     const editButton = taskLabel.querySelector('.editButton');
     editButton.addEventListener("click", () => {
-        // Lógica para edição da tarefa
+
+        const modal = document.querySelector('.modal-container');
+        const blurToDoList = document.querySelector('.borrar-fundo');
+        const editTaskInput = document.getElementById('editar-tarefa-input');
+        const closeModalButton = document.getElementById('botao-cancelar');
+        const saveButton = document.getElementById('botao-salvar');
+
+        const currentTaskText = taskLabel.querySelector('.task-text');
+        if (currentTaskText) {
+            editTaskInput.value = currentTaskText.textContent.trim();
+
+            // Abrir modal
+            modal.classList.remove('esconder');
+            blurToDoList.classList.remove('esconder');
+
+            if (closeModalButton) { // fechar modal -> cancelar edição
+                closeModalButton.addEventListener("click", () => {
+                    modal.classList.add('esconder');
+                    blurToDoList.classList.add('esconder');
+                });
+            }
+
+            // Salvar edição de tarefa
+            saveButton.addEventListener("click", () => {
+
+                // realiza a atualização do texto da tarefa após a edição
+                currentTaskText.textContent = editTaskInput.value.trim(); 
+                modal.classList.add('esconder');
+                blurToDoList.classList.add('esconder');
+            }, { once: true })
+            
+        }
     });
 };
 
@@ -52,6 +84,6 @@ const editTask = (taskLabel) => {
 const finishTask = (taskLabel) => {
     const taskCompleted = taskLabel.querySelector('.checkbox-marked');
     taskCompleted.addEventListener('click', () => {
-        taskLabel.classList.toggle('task-checked'); // Use toggle para alternar a classe
+        taskLabel.classList.toggle('task-checked'); // Usando toggle para alternar a classe
     });
 };
